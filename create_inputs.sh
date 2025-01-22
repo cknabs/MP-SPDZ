81fafca1b65188837b26e15cdcafc174fe1a6155
Christian Knabenhans <christian.knabenhans@gmail.com>
2025-01-22 08:36:44 +0100

    add dummy input generation

---
+++ create_inputs.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+source .envrc
+
+PARTY0_INPUT=Player-Data/Input-P0-0
+PARTY1_INPUT=Player-Data/Input-P1-0
+
+echo "" > $PARTY0_INPUT
+echo "" > $PARTY1_INPUT
+
+for (( i=1; i<=$SHOW_UP; i++ ))
+do 
+    B=$(($RANDOM % 2))
+    echo "$B " >> $PARTY0_INPUT
+    echo "$((1 - $B)) " >> $PARTY1_INPUT
+done
---
+++ create_inputs_2.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# source .envrc
+
+PARTY0_INPUT=Player-Data/Input-P0-0
+# PARTY1_INPUT=MP-SPDZ/Player-Data/Input-P1-0
+
+echo "" > $PARTY0_INPUT
+# echo "" > $PARTY1_INPUT
+
+echo "Generating private boolean attribute"
+for (( i=1; i<=$NUM_RECIPIENTS; i++ ))
+do 
+    B=$(($RANDOM % 2))
+    echo "$B " >> $PARTY0_INPUT
+    # echo "$((1 - $B)) " >> $PARTY1_INPUT
+done
+
+INFO_LEN=10
+echo "Generating private information attribute of length $INFO_LEN"
+for (( i=1; i<=$NUM_RECIPIENTS; i++ ))
+do 
+    for (( j=1; j<=$INFO_LEN; j++ ))
+    do 
+        N=$(($RANDOM % 10))
+        echo "$N " >> $PARTY0_INPUT
+        # echo "$((1 - $B)) " >> $PARTY1_INPUT
+    done
+done
